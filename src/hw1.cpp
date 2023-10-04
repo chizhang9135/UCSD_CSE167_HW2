e53c669f6a53bc154ba067658eb6a0cfd0cab858
Chi Zhang <chizhang9135@icloud.com>
2023-10-03 23:24:48 -0700

    part one and two done.

--- src/hw1.cpp
+++ src/hw1.cpp
@@ -2,12 +2,13 @@
 #include "hw1_scenes.h"
 
 using namespace hw1;
-
+// TODO: office hours question: < or <= for determining whether a pixel is inside a shape
 Image3 hw_1_1(const std::vector<std::string> &params) {
     // Homework 1.1: render a circle at the specified
     // position, with the specified radius and color.
 
     Image3 img(640 /* width */, 480 /* height */);
+
 
     Vector2 center = Vector2{img.width / 2 + Real(0.5), img.height / 2 + Real(0.5)};
     Real radius = 100.0;
@@ -27,9 +28,20 @@ Image3 hw_1_1(const std::vector<std::string> &params) {
         }
     }
 
+    Vector3 background_color = {0.5, 0.5, 0.5};
+
     for (int y = 0; y < img.height; y++) {
         for (int x = 0; x < img.width; x++) {
-            img(x, y) = Vector3{1, 1, 1};
+            // calculate the distance from the center of the circle
+            Vector2 pixel_center = Vector2{x + Real(0.5), y + Real(0.5)};
+            Real distance = length(pixel_center - center);
+
+            // if the distance is less than the radius, the pixel is inside the circle
+            if (distance < radius) {
+                img(x, y) = color;
+            } else {
+                img(x, y) = background_color;
+            }
         }
     }
     return img;
@@ -46,9 +58,31 @@ Image3 hw_1_2(const std::vector<std::string> &params) {
 
     Image3 img(scene.resolution.x, scene.resolution.y);
 
+    // fill the image with the background color
     for (int y = 0; y < img.height; y++) {
         for (int x = 0; x < img.width; x++) {
-            img(x, y) = Vector3{1, 1, 1};
+            img(x, y) = scene.background;
+        }
+    }
+
+    // for bonus points, by box bounding, reduce unnecessary pixel calculations
+    for (const Circle &circle : scene.objects) {
+        Vector2 bounding_box_min = circle.center - Vector2{circle.radius, circle.radius};
+        Vector2 bounding_box_max = circle.center + Vector2{circle.radius, circle.radius};
+
+        int x_start = std::max(0, static_cast<int>(bounding_box_min.x));
+        int x_end = std::min(img.width, static_cast<int>(bounding_box_max.x));
+        int y_start = std::max(0, static_cast<int>(bounding_box_min.y));
+        int y_end = std::min(img.height, static_cast<int>(bounding_box_max.y));
+
+        for (int y = y_start; y <= y_end; y++) {
+            for (int x = x_start; x <= x_end; x++) {
+                Vector2 pixel_center = Vector2{x + Real(0.5), y + Real(0.5)};
+                Real distance = length(pixel_center - circle.center);
+                if (distance < circle.radius) {
+                    img(x, y) = circle.color;
+                }
+            }
         }
     }
     return img;
